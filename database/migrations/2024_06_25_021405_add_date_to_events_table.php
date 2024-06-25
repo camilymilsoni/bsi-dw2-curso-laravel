3b555055b43286cad5b5bd04513bd259e0db5478
Camily Milsoni <camily_milsoni@hotmail.com>
2024-06-24 23:37:54 -0300

    Aula 20 - Correção

--- database/migrations/2024_06_25_021405_add_date_to_events_table.php
+++ database/migrations/2024_06_25_021405_add_date_to_events_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->dateTime('date');
+            $table->datetime('date')->nullable()->change();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->dropColumn('date');
+            $table->datetime('date')->nullable(false)->change();
         });
     }
 };
