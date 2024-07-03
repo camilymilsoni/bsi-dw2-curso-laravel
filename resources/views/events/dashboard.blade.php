8b26cf31bc18025d0a012a36dfce661737e87d28
Camily Milsoni <camily_milsoni@hotmail.com>
2024-07-03 19:49:47 -0300

    Aula 30

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -20,20 +20,20 @@
         </thead>
         <tbody>
             @foreach($events as $event)
-            <tr>
-                <td scropt="row">{{ $loop->index + 1 }}</td>
-                <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
-                <td>{{ count($event->users) }}</td>
-                <td>
-                    <a href="/events/edit/{{ $event->id }}" class="btn btn-info edit-btn"><ion-icon name="create-outline"></ion-icon> Editar</a>
-                    <form action="/events/{{ $event->id }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger delete-btn"><ion-icon name="trash-outline"></ion-icon> Deletar</button>
-                    </form>
-                </td>
-            </tr>
-            @endforeach
+                <tr>
+                    <td scropt="row">{{ $loop->index + 1 }}</td>
+                    <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
+                    <td>{{ count($event->users) }}</td>
+                    <td>
+                        <a href="/events/edit/{{ $event->id }}" class="btn btn-info edit-btn"><ion-icon name="create-outline"></ion-icon> Editar</a> 
+                        <form action="/events/{{ $event->id }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger delete-btn"><ion-icon name="trash-outline"></ion-icon> Deletar</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach    
         </tbody>
     </table>
     @else
@@ -44,31 +44,38 @@
     <h1>Eventos que estou participando</h1>
 </div>
 <div class="col-md-10 offset-md-1 dashboard-events-container">
-    @if(count($eventsasparticipant) > 0)
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Participantes</th>
-                <th scope="col">Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($eventsasparticipant as $event)
+@if(count($eventsasparticipant) > 0)
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Participantes</th>
+            <th scope="col">Ações</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($eventsasparticipant as $event)
             <tr>
                 <td scropt="row">{{ $loop->index + 1 }}</td>
                 <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
                 <td>{{ count($event->users) }}</td>
                 <td>
-                    <a href="#">Sair do evento</a>
+                    <form action="/events/leave/{{ $event->id }}" method="POST">
+                        @csrf
+                        @method("DELETE")
+                        <button type="submit" class="btn btn-danger delete-btn">
+                            <ion-icon name="trash-outline"></ion-icon> 
+                            Sair do Evento
+                        </button>
+                    </form>
                 </td>
             </tr>
-            @endforeach
-        </tbody>
-    </table>
-    @else
-    <p>Você ainda não está participando de nenhum evento, <a href="/">veja todos os eventos</a></p>
-    @endif
+        @endforeach    
+    </tbody>
+</table>
+@else
+<p>Você ainda não está participando de nenhum evento, <a href="/">veja todos os eventos</a></p>
+@endif
 </div>
 @endsection
