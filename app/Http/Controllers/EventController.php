b7e444a955da8e582be1f947a6dee10b6e55d682
Camily Milsoni <camily_milsoni@hotmail.com>
2024-07-02 23:13:03 -0300

    Aula 24

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -69,7 +70,9 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
-        return view('events.show', ['event' => $event]);
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
+
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
         
     }
 }
