6aa7690a95b84294f93adb68973c774c81e57221
Camily Milsoni <camily_milsoni@hotmail.com>
2024-07-03 19:34:25 -0300

    Aula 29

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -82,7 +82,11 @@ class EventController extends Controller
 
         $events = $user->events;
 
-        return view('events.dashboard', ['events' => $events]);
+        $eventsAsParticipant = $user->eventsAsParticipant;
+
+        return view('events.dashboard', 
+            ['events' => $events, 'eventsasparticipant' => $eventsAsParticipant]
+        );
 
     }
 
@@ -96,7 +100,13 @@ class EventController extends Controller
 
     public function edit($id) {
 
+        $user = auth()->user();
+
         $event = Event::findOrFail($id);
+
+        if($user->id != $event->user_id) {
+            return redirect('/dashboard');
+        }
 
         return view('events.edit', ['event' => $event]);
 
